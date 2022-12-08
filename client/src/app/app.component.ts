3ad22325075956f955718987f0614d595b1bb74b
Leonardo <leonardo.fernandes98@hotmail.com>
2022-12-08 12:33:14 -0300

    feat(client): Added the HttpClientModule to be able to make http requests in the client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Social Network App in Angular and .NET7';
+
+export class AppComponent implements OnInit {
+  title: string = 'Social Network App in Angular and .NET7';
+  users: any;
+
+  constructor(private http: HttpClient)
+  {    
+  }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/v1/users").subscribe(
+      {
+        next: response => this.users = response,
+        error: error => console.log(error),
+        complete: () => console.log('Request completed!')
+      }
+    );
+  }
 }
